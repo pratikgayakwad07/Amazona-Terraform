3ac1d5ed3c4c399fd8ea870a4509a2546c100ea7
pratikgayakwad07 <148746650+pratikgayakwad07@users.noreply.github.com>
2026-05-19 19:29:05 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-bucket-127"
-    key            = "amazona-app/terraform.tfstate"
+    key            = "backend/terraform.tfstate"
     region         = "ap-south-1"
     use_lockfile   =  true
   }
